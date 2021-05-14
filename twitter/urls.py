3c500f79f1c5f52b00b52d3aedd44d98c4c39216
Ping Lu <pinglu2015@gmail.com>
2021-05-13 22:31:04 -0700

    add debug tool bar

--- twitter/urls.py
+++ twitter/urls.py
@@ -15,14 +15,23 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
 from rest_framework import routers
 from accounts.api import views
 
 router = routers.DefaultRouter()
 router.register(r'api/users', views.UserViewSet)
+router.register(r'api/accounts', views.AccountViewSet, basename='accounts')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns.append(
+        path('__debug__', include(debug_toolbar.urls))
+    )
+
