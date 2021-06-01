cdc164c3be1abd4f29978bc3b8289c6da53f24b0
Ping Lu <pinglu2015@gmail.com>
2021-06-01 10:00:29 -0700

    update formate for like model Meta

--- likes/models.py
+++ likes/models.py
@@ -15,7 +15,11 @@ class Like(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        unique_together = (('user', 'content_type', 'object_id'), )
-        index_together = (('content_type', 'object_id', 'created_at'),)
+        unique_together = (
+            ('user', 'content_type', 'object_id'),
+        )
+        index_together = (
+            ( 'content_type', 'object_id', 'created_at'),
+        )
     def __str__(self):
         return f'{self.created_at} - {self.user} liked {self.content_type} {self.object_id}'
