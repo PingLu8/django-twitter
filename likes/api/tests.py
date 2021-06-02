87dd6f7bc1219d9d64603e02f269975254ee7b48
Ping Lu <pinglu2015@gmail.com>
2021-06-01 20:56:32 -0700

    add like cancel api and unit test

--- likes/api/tests.py
+++ likes/api/tests.py
@@ -1,6 +1,7 @@
 from testing.testcases import TestCase
 
 LIKE_BASE_URL = '/api/likes/'
+LIKE_CANCEL_URL = '/api/likes/cancel/'
 
 class LikeApiTests(TestCase):
 
@@ -60,3 +61,51 @@ class LikeApiTests(TestCase):
         self.assertEqual(comment.like_set.count(), 1)
         self.dongxie_client.post(LIKE_BASE_URL, data)
         self.assertEqual(comment.like_set.count(), 2)
+
+    def test_cancel(self):
+        tweet = self.create_tweet(self.linghu)
+        comment = self.create_comment(self.dongxie, tweet)
+        like_comment_data = {'content_type': 'comment', 'object_id': comment.id}
+        like_tweet_data = {'content_type': 'tweet', 'object_id': tweet.id}
+        self.linghu_client.post(LIKE_BASE_URL, like_comment_data)
+        self.dongxie_client.post(LIKE_BASE_URL, like_tweet_data)
+        self.assertEqual(tweet.like_set.count(), 1)
+        self.assertEqual(comment.like_set.count(), 1)
+
+        response = self.anonymous_client.post(LIKE_CANCEL_URL, like_comment_data)
+        self.assertEqual(response.status_code, 403)
+
+        response = self.linghu_client.get(LIKE_CANCEL_URL, like_comment_data)
+        self.assertEqual(response.status_code, 405)
+
+        response = self.linghu_client.post(LIKE_CANCEL_URL, {
+            'content_type': 'wrongtype',
+            'object_id': tweet.id,
+        })
+        self.assertEqual(response.status_code, 400)
+
+        response = self.linghu_client.post(LIKE_CANCEL_URL, {
+            'content_type': 'comment',
+            'object_id': -1,
+        })
+        self.assertEqual(response.status_code, 400)
+
+        response = self.dongxie_client.post(LIKE_CANCEL_URL, like_comment_data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(comment.like_set.count(), 1)
+        self.assertEqual(tweet.like_set.count(), 1)
+
+        response = self.linghu_client.post(LIKE_CANCEL_URL, like_comment_data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(comment.like_set.count(), 0)
+        self.assertEqual(tweet.like_set.count(), 1)
+
+        response = self.linghu_client.post(LIKE_CANCEL_URL, like_tweet_data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(comment.like_set.count(), 0)
+        self.assertEqual(tweet.like_set.count(), 1)
+
+        response = self.dongxie_client.post(LIKE_CANCEL_URL, like_tweet_data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(comment.like_set.count(), 0)
+        self.assertEqual(tweet.like_set.count(), 0)
