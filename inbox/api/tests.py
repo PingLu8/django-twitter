44167405bf3d30948576bdfe3bb9f069c8be8117
Ping Lu <pinglu2015@gmail.com>
2021-06-08 09:33:27 -0700

    add notification create api

--- inbox/api/tests.py
+++ inbox/api/tests.py
@@ -26,3 +26,58 @@ class NotificationTests(TestCase):
             'object_id': self.dongxie_tweet.id,
         })
         self.assertEqual(Notification.objects.count(), 1)
+
+class NotificationApiTests(TestCase):
+    def setUp(self):
+        self.linghu, self.linghu_client = self.create_user_and_client('linghu')
+        self.dongxie, self.dongxie_client = self.create_user_and_client('dongxie')
+        self.linghu_tweet = self.create_tweet(self.linghu)
+
+    def test_unread_count(self):
+        self.dongxie_client.post(LIKE_URL, {
+            'content_type': 'tweet',
+            'object_id': self.linghu_tweet.id,
+        })
+
+        url = '/api/notifications/unread-count/'
+        response = self.linghu_client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['unread_count'], 1)
+
+        comment = self.create_comment(self.linghu, self.linghu_tweet)
+        self.dongxie_client.post(LIKE_URL, {
+            'content_type': 'comment',
+            'object_id': comment.id,
+        })
+        response = self.linghu_client.get(url)
+        self.assertEqual(response.data['unread_count'], 2)
+        response = self.dongxie_client.get(url)
+        self.assertEqual(response.data['unread_count'], 0)
+
+    def test_mark_all_as_read(self):
+        self.dongxie_client.post(LIKE_URL, {
+            'content_type': 'tweet',
+            'object_id': self.linghu_tweet.id,
+        })
+        comment = self.create_comment(self.linghu, self.linghu_tweet)
+        self.dongxie_client.post(LIKE_URL, {
+            'content_type': 'comment',
+            'object_id': comment.id,
+        })
+
+        unread_url = '/api/notifications/unread-count/'
+        response = self.linghu_client.get(unread_url)
+        self.assertEqual(response.data['unread_count'], 2)
+
+        mark_url = '/api/notifications/mark-all-as-read/'
+        response = self.linghu_client.get(mark_url)
+        self.assertEqual(response.status_code, 405)
+        response = self.linghu_client.post(mark_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['marked_count'], 2)
+        response = self.linghu_client.get(unread_url)
+        self.assertEqual(response.data['unread_count'], 0)
+
+
+
+
