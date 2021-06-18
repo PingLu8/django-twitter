a1bd3234f0002cbef3721d6cb59b89960f417fc6
Ping Lu <pinglu2015@gmail.com>
2021-06-17 19:50:48 -0700

    add EndlessPagination and apply on Tweets list view

--- utils/paginations.py
+++ utils/paginations.py
@@ -1,20 +1,36 @@
-from rest_framework.pagination import PageNumberPagination
-from rest_framework.response import Response
+from rest_framework.pagination import BasePagination
+from rest_framework.pagination import Response
 
-class FriendshipPagination(PageNumberPagination):
-    # https://.../api/friendships/1/followers/?page=3&size=10
-    # Default page_size when page is not in url query parameters.
+class EndlessPagination(BasePagination):
     page_size = 20
-    # Default page_size_query_param is None. It means customer can't specify the page size
-    # Adding this config, customer can specify the size for different scenarios such as web or phone.
-    page_size_query_param = 'size'
-    max_page_size = 20
+
+    def __init__(self):
+        super(EndlessPagination, self).__init__()
+        self.has_next_page = False
+
+    def to_html(self):
+        pass
+
+    def paginate_queryset(self, queryset, request, view=None):
+        if 'created_at__gt' in request.query_params:
+            # created_at__gt is used for pull-refresh.
+            # For convenience, pull-refresh doesn't use pagination but use download and refresh all data.
+            # For data haven't updated for a long time, we don't use pull-refresh style, but use download to update with the latest data.
+            created_at__gt = request.query_params['created_at__gt']
+            queryset = queryset.filter(created_at__gt=created_at__gt)
+            self.has_next_page=False
+            return queryset.order_by('-created_at')
+
+        if 'created_at__lt' in request.query_params:
+            created_at__lt = request.query_params['created_at__lt']
+            queryset = queryset.filter(created_at__lt=created_at__lt)
+
+        queryset = queryset.order_by('-created_at')[:self.page_size + 1]
+        self.has_next_page = (len(queryset) > self.page_size)
+        return queryset[:self.page_size]
 
     def get_paginated_response(self, data):
         return Response({
-                'total_results': self.page.paginator.count,
-                'total_pages': self.page.paginator.num_pages,
-                'page_number': self.page.number,
-                'has_next_page': self.page.has_next(),
+                'has_next_page': self.has_next_page,
                 'results': data,
         })
