fceff32ca9d6d37b48b72531fef25739750d6ce5
Ping Lu <pinglu2015@gmail.com>
2021-07-29 19:23:35 -0700

    add newsfeed to cache in redis

--- newsfeeds/api/views.py
+++ newsfeeds/api/views.py
@@ -1,9 +1,10 @@
+from newsfeeds.api.serializers import NewsFeedSerializer
+from newsfeeds.models import NewsFeed
+from newsfeeds.services import NewsFeedService
 from rest_framework import viewsets, status
 from rest_framework.decorators import permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from newsfeeds.models import NewsFeed
-from newsfeeds.api.serializers import NewsFeedSerializer
 from utils.paginations import EndlessPagination
 
 class NewsFeedViewSet(viewsets.GenericViewSet):
@@ -11,7 +12,7 @@ class NewsFeedViewSet(viewsets.GenericViewSet):
     pagination_class = EndlessPagination
 
     def list(self, request):
-        queryset = NewsFeed.objects.filter(user=self.request.user)
-        page = self.paginate_queryset(queryset)
+        newsfeeds = NewsFeedService.get_cached_newsfeeds(request.user.id)
+        page = self.paginate_queryset(newsfeeds)
         serializer = NewsFeedSerializer(page, context={'request':request}, many=True,)
         return self.get_paginated_response(serializer.data)
