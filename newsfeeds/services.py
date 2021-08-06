25319d9777205d1b15259bc282091f010ab8cbcb
PingLu8 <82852382+PingLu8@users.noreply.github.com>
2021-08-06 09:08:07 -0700

    set up celery for message queue (#37)

--- newsfeeds/services.py
+++ newsfeeds/services.py
@@ -1,28 +1,24 @@
-from newsfeeds.models import NewsFeed
 from friendships.services import FriendshipService
+from newsfeeds.models import NewsFeed
+from newsfeeds.tasks import fanout_newsfeeds_task
 from twitter.cache import USER_NEWSFEEDS_PATTERN
 from utils.redis_helper import RedisHelper
-
 
 class NewsFeedService(object):
 
     @classmethod
     def fanout_to_followers(cls, tweet):
-        # wrong implement, can't put db action into For loop. Too slow
-        # followers = FriendshipService.get_followers(tweet.user)
-        # for follower in followers:
-        #     NewsFeed.objects.create(user=follower, tweet=tweet)
-
-        newsfeeds = [
-            NewsFeed(user=follower, tweet=tweet)
-            for follower in FriendshipService.get_followers(tweet.user)
-        ]
-        newsfeeds.append(NewsFeed(user=tweet.user, tweet=tweet))
-        NewsFeed.objects.bulk_create(newsfeeds)
-
-        # bulk create can't generate post_save signal, so need to manually push newsfeeds to cache
-        for newsfeed in newsfeeds:
-            cls.push_newsfeed_to_cache(newsfeed)
+        # this is to create a fanout task in message queue configured by celery
+        # parameter is tweet. any worker monitoring the message query is able to get the task
+        # the worker process will execute fanout_newsfeeds_task() to complete the async task
+        # if the task need 10s, it will spend on worker process,
+        # not spend on sending tweet process. so delay will be execute and finish immidiately.
+        # note: delay parameter must be a value that can be celery to serialize
+        # because worker process is independent process, even another machine.
+        # so, it can't know what is value in memory of the web process.
+        # thus, we send tweet.id as parameter instead of tweet. because celery doesn't
+        # know how to serialize Tweet.
+        fanout_newsfeeds_task.delay(tweet.id)
 
     @classmethod
     def get_cached_newsfeeds(cls, user_id):
